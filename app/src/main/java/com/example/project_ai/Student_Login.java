17c8a82c4a447ba085f83344288e7806b0e76331
ParthPatil-23 <parthpatil2823@gmail.com>
2022-09-08 13:16:38 +0530

    Firebase Connected to the project, all the data is uplaoded to firebase in json and Student and teacher Authentication is working fine.

--- app/src/main/java/com/example/project_ai/Student_Login.java
+++ app/src/main/java/com/example/project_ai/Student_Login.java
@@ -1,26 +1,135 @@
 package com.example.project_ai;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Student_Login extends AppCompatActivity {
 
-    Button Go_back;
+    Button Go_back,Student_login;
+    EditText Student_ID, Student_Password;
+    ProgressDialog progressDialog;
+
+    private FirebaseDatabase database;
+    private DatabaseReference databaseReference;
+
+    // Firebase mAuth and mUSer
+    FirebaseAuth mAuth;
+    FirebaseUser mUser;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_login);
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+        
+        // initializing Button
         Go_back = (Button)findViewById(R.id.Go_back);
+        Student_login = (Button)findViewById(R.id.Student_login);
+
+        // Initializing Input field Variables
+        Student_ID = findViewById(R.id.Student_ID);
+        Student_Password = findViewById(R.id.Student_Password);
+
+        // Initializing Database
+        databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://arwachin-india-3bb19-default-rtdb.firebaseio.com/");
+
+
+
 
 
         Go_back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(Student_Login.this, MainActivity.class));
+            }
+        });
+
+
+        Student_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PerformLogin();
+
+            }
+
+            private void PerformLogin() {
+                String Std_ID = Student_ID.getText().toString();
+                String Std_pass = Student_Password.getText().toString();
+
+
+                if(Std_ID.isEmpty()){
+                    Student_ID.setError("Incorrect Student I.D");
+                    Student_ID.requestFocus();
+
+                }else if(Std_pass.isEmpty()){
+                    Student_Password.setError("Incorrect Password");
+                    Student_Password.requestFocus();
+
+                }else if(Std_pass.length()<6){
+                    Student_Password.setError("Incorrect Password");
+                    Student_Password.requestFocus();
+
+                }else{
+                    databaseReference.child("Student").addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                            // Check if Student ID exists in firebase realtime datatbase
+                            if(snapshot.hasChild(Std_ID)){
+
+                                // Student ID exists
+                                // checking if the user entered the correct password
+                                final String getPassword = snapshot.child(Std_ID).child("Password").getValue(String.class);
+
+                                if(getPassword.equals(Std_pass)){
+                                    Toast.makeText(Student_Login.this, "Login Successfull", Toast.LENGTH_SHORT).show();
+
+                                    // Opening Student Navigation
+                                    startActivity(new Intent(Student_Login.this,Student_Nav.class));
+                                }
+                                else{
+                                    Student_Password.setError("Incorrect Password");
+                                    Student_Password.requestFocus();
+                                }
+                            }
+                            else{
+                                Student_ID.setError("Incorrect Student I.D");
+                                Student_ID.requestFocus();
+                                Toast.makeText(Student_Login.this, "Student I.D Doesn't Exist", Toast.LENGTH_SHORT).show();
+                            }
+
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+                }
+
+
+
+
             }
         });
 
--- app/src/main/java/com/example/project_ai/Teacher_Login.java
+++ app/src/main/java/com/example/project_ai/Teacher_Login.java
@@ -1,21 +1,62 @@
 package com.example.project_ai;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+
 
 public class Teacher_Login extends AppCompatActivity {
 
-    Button Go_back;
+    Button Go_back,Teacher_login;
+    EditText Teacher_ID, Teacher_Password;
+    ProgressDialog progressDialog;
+
+    private FirebaseDatabase database;
+    private DatabaseReference databaseReference;
+
+    // Firebase mAuth and mUSer
+    FirebaseAuth mAuth;
+    FirebaseUser mUser;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teacher_login);
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+
+        // initializing Button
         Go_back = (Button)findViewById(R.id.Go_back);
+        Teacher_login = (Button)findViewById(R.id.Teacher_login);
+
+        // Initializing Input field Variables
+        Teacher_ID = findViewById(R.id.Teacher_ID);
+        Teacher_Password = findViewById(R.id.Teacher_Password);
+
+        // Initializing Database
+        databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://arwachin-india-3bb19-default-rtdb.firebaseio.com/");
+
+
+
+
+
 
 
         Go_back.setOnClickListener(new View.OnClickListener() {
@@ -25,6 +66,76 @@ public class Teacher_Login extends AppCompatActivity {
 
             }
         });
+
+
+        Teacher_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PerformLogin();
+
+            }
+
+            private void PerformLogin() {
+                String Teach_ID = Teacher_ID.getText().toString();
+                String Teach_pass = Teacher_Password.getText().toString();
+
+
+                if(Teach_ID.isEmpty()){
+                    Teacher_ID.setError("Incorrect Teacher I.D");
+                    Teacher_ID.requestFocus();
+
+                }else if(Teach_pass.isEmpty()){
+                    Teacher_Password.setError("Incorrect Password");
+                    Teacher_Password.requestFocus();
+
+                }else if(Teach_pass.length()<6){
+                    Teacher_Password.setError("Incorrect Password");
+                    Teacher_Password.requestFocus();
+
+                }else{
+                    databaseReference.child("Teacher").addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                            // Check if Teacher ID exists in firebase realtime database
+                            if(snapshot.hasChild(Teach_ID)){
+
+                                // Teacher ID exists
+                                // checking if the user entered the correct password
+                                final String getPassword = snapshot.child(Teach_ID).child("Password").getValue(String.class);
+
+                                if(getPassword.equals(Teach_pass)){
+                                    Toast.makeText(Teacher_Login.this, "Login Successfull", Toast.LENGTH_SHORT).show();
+
+                                    // Opening Teacher Navigation
+                                    startActivity(new Intent(Teacher_Login.this,Teacher_Nav.class));
+                                }
+                                else{
+                                    Teacher_Password.setError("Incorrect Password");
+                                    Teacher_Password.requestFocus();
+                                }
+                            }
+                            else{
+                                Teacher_ID.setError("Incorrect Teacher I.D");
+                                Teacher_ID.requestFocus();
+                                Toast.makeText(Teacher_Login.this, "Teacher I.D Doesn't Exist", Toast.LENGTH_SHORT).show();
+                            }
+
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+                }
+
+
+
+
+            }
+        });
+
 
     }
 }
